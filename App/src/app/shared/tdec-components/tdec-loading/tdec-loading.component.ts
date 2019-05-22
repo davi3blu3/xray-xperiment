8c2a8273f0fdb6449b132307b88e411bb31e7ecb
Dave Harned <dharned@gmail.com>
2019-05-22 11:11:06 -0500

    build loading animation component

---
+++ App/src/app/shared/tdec-components/tdec-loading/tdec-loading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tdec-loading',
+  templateUrl: './tdec-loading.component.html',
+  styleUrls: ['./tdec-loading.component.scss']
+})
+export class TdecLoadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
